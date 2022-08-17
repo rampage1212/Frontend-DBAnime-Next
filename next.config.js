1101fb4a281502ab552246252c55b1427977fd81
rampage0131 <jake.docker0131@gmail.com>
2022-08-18 02:30:40 +0700

    fixing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n:{
-    locales: ['en-US', 'id-ID'],
-    defaultLocale: 'id-ID',
-    localeDetection: true
-  }
+  // i18n:{
+  //   locales: ['en-US', 'id-ID'],
+  //   defaultLocale: 'id-ID',
+  //   localeDetection: true
+  // }
 }
 
 module.exports = nextConfig
